f1ebf8011cb8f5b7ad4b7abdfbb7139ebcdbbdaf
Katia Midori <69910600+katchion13@users.noreply.github.com>
2021-06-28 01:10:35 +0900

    Add files via upload

---
+++ formulario-bootstrap/conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$servidor = "localhost";
+	$usuario = "katchi47_midori";
+	$senha = "[J85^OL;YTm7";
+	$dbname = "katchi47_wp611";
+	
+	//Criar a conexao
+	$con = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+?>
